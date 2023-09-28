e905228f3c296b5a9aec2dc133e1a9563be02db9
Nihal Chowdhury <nihal.c614@gmail.com>
2023-09-27 18:28:57 -0700

    Basic Freddy Movement and response

--- Scripts/Enemy/Melvinzord.gd
+++ Scripts/Enemy/Melvinzord.gd
@@ -27,7 +27,7 @@ const MOVEMENT_INTERVAL : float = 3.97
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_pos = 0
-	AI_level = 10
+	AI_level = 5
 	# AI_level = night_AI_levels[game.current_night - 1]
 	
 	$Timer.wait_time = MOVEMENT_INTERVAL
@@ -41,59 +41,59 @@ func _process(delta):
 	if waiting_to_move:
 		time_left -= delta
 		freddy_countdown.text = "Time Left: %.02f" % time_left
-	else:
-		time_left = current_countdown
 	pass
-
-func set_freddy_location(pos : int) -> void:
-	set_global_position(enemy_locations[pos].global_position)
-	set_global_rotation(enemy_rotations[pos])
 
 func _on_timer_timeout():
 	if !enabled:
 		return
 	
-	if game.watching_cam && !waiting_to_move:
-		print("FREDDY -- AUTO FAIL")
+	if waiting_to_move:
+		print("FREDDY -- ALREADY TRYING TO MOVE")
+		return
+	
+	if game.watching_cam:
+		#print("FREDDY -- AUTO FAIL")
+		return
+	
+	if !bonnie_left_spawn:
+		#print("FREDDY -- Can't move anyway")
 		return
 	
 	rand.randomize()
 	var check = rand.randi_range(1,20)
-	if AI_level >= check && !waiting_to_move: 
+	if AI_level >= check:
+		print("FREDDY -- %s VS %s: LET'S GET MOVING" % [AI_level, check])
+		# Figure out where to move to
 		current_pos += 1
 		
+		# Start the countdown for moving
 		current_countdown = determine_countdown_duration(AI_level)
 		time_left = current_countdown
+		waiting_to_move = true
+		await wait_to_move()
 		
-		await get_tree().create_timer(current_countdown).timeout
-		
-		if !game.watching_cams:
-			waiting_to_move = false
-		else:
-			await wait_to_move()
-		
-		if bonnie_left_spawn:
-			print("FREDDY -- %s VS %s: MOVING TO %s" % [AI_level, check, current_pos])
-			#move_freddy(current_pos)
-		else:
-			print("FREDDY -- %s VS %s: TRY AGAIN LATER" % [AI_level, check, current_pos])
+		print("FREDDY -- MOVE TO %s" % current_pos)
+		set_freddy_location(current_pos)
+	else:
+		print("FREDDY -- %s VS %s: FAILED" % [AI_level, check])
+
+func set_freddy_location(pos : int) -> void:
+	set_global_position(enemy_locations[pos].global_position)
+	#set_global_rotation(enemy_rotations[pos])
 
 func wait_to_move():
-	print("FREDDY -- WAITING TO MOVE")
-	await $"../Player".watching_office
-	print("FREDDY -- GO GO GO!")
+	#print("FREDDY -- WAITING FOR %.02fs" % current_countdown)
+	await get_tree().create_timer(current_countdown).timeout
+	
+	if game.watching_cam:
+		await $"../Player".watching_office
+		print("FREDDY -- NOT WATCHING CAMS")
+	
+	#print("FREDDY -- READY TO MOVE!")
 	waiting_to_move = false
 
 func determine_countdown_duration(level : int) -> float:
 	return 15.0 - ((5.0/3.0) * (level - 1))
-
-func _on_player_camera_state_change(_watching):
-	pass
-	"""
-	if !watching:
-		waiting_to_move = false
-		move_freddy(current_pos)
-	"""
 
 func _on_shashumga_bonnie_left_spawn():
 	if bonnie_left_spawn:
