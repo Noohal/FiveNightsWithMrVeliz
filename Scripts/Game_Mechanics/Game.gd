7edc7901950c020edcc613df3ac89f0c283b84ef
Nihal Chowdhury <nihal.c614@gmail.com>
2023-10-29 04:45:09 -0700

    Added Freddy Jumpscare 2

--- Scripts/Game_Mechanics/Game.gd
+++ Scripts/Game_Mechanics/Game.gd
@@ -14,13 +14,78 @@ var current_night : int
 
 var getting_scared : bool = false
 var freddy_can_attack : bool = false
+var power_loss_jumpscare : bool = false
 
 signal getting_killed
 
 func _ready():
 	await Global.ready
-	current_night = Global.current_night
+	#current_night = Global.current_night
+	current_night = 2
 	NightLabel.text = "Night " + str(current_night+1)
+
+var elapsed_time : float = 0.0
+var accumulated_time : float = 0.0
+
+var freddy_playing_music_box : bool = false
+
+func _process(delta):
+	if !power_loss_jumpscare:
+		return
+	elapsed_time += delta
+	accumulated_time += delta
+	
+	if accumulated_time >= 20.0:
+		if freddy_playing_music_box:
+			await get_tree().create_timer(1.2).timeout
+			freddy_power_jumpscare()
+		else:
+			turn_on_the_music()
+		elapsed_time = 0.0
+	elif elapsed_time >= 5.0:
+		if !freddy_playing_music_box:
+			if freddy_appears():
+				turn_on_the_music()
+		else:
+			if turn_off_music():
+				freddy_power_jumpscare()
+		elapsed_time = 0.0
+
+func freddy_appears() -> bool:
+	rand.randomize()
+	if rand.randi_range(1,5) <= 2:
+		return true
+	return false
+
+func turn_on_the_music() -> void:
+	$Melvinzord.set_freddy_location(7)
+	$HarHarHarHar.play()
+	freddy_playing_music_box = true
+	accumulated_time = 0.0
+
+func turn_off_music() -> bool:
+	rand.randomize()
+	if rand.randi_range(1,5) <= 2:
+		return true
+	return false
+
+func freddy_power_jumpscare() -> void:
+	$HarHarHarHar.stop()
+	$Melvinzord.set_freddy_location(6)
+	await get_tree().create_timer(randf_range(3.0, 5.0)).timeout
+	$"Map/Scareroom/ScareCam".make_current()
+	turn_off_hud()
+	$"Melvinzord/AnimationPlayer".play("change_into_super_toilet")
+	exit_game()
+	
+func turn_off_hud():
+	UI.visible = false
+	player_camera_bar.visible = false
+	emit_signal("getting_killed")
+
+func exit_game():
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://Scenes/GameOver.tscn")
 
 func _on_left_door_left_door_change(active):
 	left_door_close = active
@@ -73,25 +138,19 @@ func _on_stonedome_jumpscare_foxy():
 	$"Stonedome/AnimationPlayer".play("stage_5_running")
 	#$"Jumpscare1".play()
 	exit_game()
-
-func turn_off_hud():
-	UI.visible = false
-	player_camera_bar.visible = false
-	emit_signal("getting_killed")
-
-func exit_game():
-	await get_tree().create_timer(2).timeout
-	get_tree().change_scene_to_file("res://Scenes/GameOver.tscn")
-
+	
 func _on_player_camera_state_change(watching):
 	watching_cam = watching
 
 func _on_power_power_loss():
 	getting_scared = true
+	power_loss_jumpscare = true
 	
-	await get_tree().create_timer(3).timeout
-	$"Map/Scareroom/ScareCam".make_current()
-	turn_off_hud()
-	$"Melvinzord/AnimationPlayer".play("change_into_super_toilet")
-	#$"Jumpscare1".play()
-	exit_game()
+	$"Shashumga".enabled = false
+	$"Stonedome".enabled = false
+	$"Cabron".enabled = false
+	
+	var dim_value = 0.1
+	$"Office/Room Light Left".set_param(Light3D.PARAM_ENERGY, dim_value)
+	$"Office/Room Light Middle".set_param(Light3D.PARAM_ENERGY, dim_value)
+	$"Office/Room Light Left2".set_param(Light3D.PARAM_ENERGY, dim_value)
--- Scripts/Game_Mechanics/Power.gd
+++ Scripts/Game_Mechanics/Power.gd
@@ -20,10 +20,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var current_time = Time.get_unix_time_from_system()	
-	var power_decrease : float = (drain_rate * 0.01) * (1.0 / (float)(2**(power_usage - 1)))
+	var power_decrease : float = (drain_rate * 10) * (1.0 / (float)(2**(power_usage - 1)))
 	
 	if current_time - last_time >= drain_time:
-		print("%.02f - %.02f = %.02f" % [power_level, power_decrease, power_level - power_decrease])
+		#print("%.02f - %.02f = %.02f" % [power_level, power_decrease, power_level - power_decrease])
 		power_level -= power_decrease
 		last_time = current_time
 	
